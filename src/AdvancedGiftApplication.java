71e2e3b183a6e190c77c175c953d157f51afe2da
Mehran <36104315+Mehran-at@users.noreply.github.com>
2018-11-29 10:35:18 +0100

    Update AdvancedGiftApplication.java

--- src/AdvancedGiftApplication.java
+++ src/AdvancedGiftApplication.java
@@ -1,3 +1,14 @@
+// Christmas is coming and Derek’s new working colleges want to organize an “invisible friend” present giving in their company.
+// . Create the AdvancedGiftApplication class and write the following instructions in its main method:
+// . Create the Box class that has a String called content.
+// . Create an empty giftBox Optional of Box and display it.
+// . Create a Box with a present name as content.
+// . Assign an Optional of Box to the giftBox containing the previous Box and display it.
+// . Ask if the giftBox is present and display the answer.
+// . Get the value contained in giftBox and display it. To display the Box class create the toString method.
+//   Hints:
+//    > Use Intellij shortcuts to create the toString and equals methods.
+
 import java.util.Optional;
 
 public class AdvancedGiftApplication {
